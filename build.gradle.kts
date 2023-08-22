8b5cd296274731e9e4b98a54a3525112c9c8bdad
Jochem <jochemminecraftyt@gmail.com>
2023-08-22 13:40:20 +0200

    allow publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    `maven-publish`
 }
 
 group = "nl.jochem"
@@ -24,5 +25,17 @@ kotlin {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("PacketManagerKt")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.properties["group"] as? String?
+            artifactId = project.name
+            version = project.properties["version"] as? String?
+
+            from(components["java"])
+        }
+    }
 }
